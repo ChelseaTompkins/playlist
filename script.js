72fad0ae0588584744e13124359de2ddb1e462b1
Chelsea <cnoelle1091@gmail.com>
2018-07-12 15:25:51 -0700

    Populated album images to second page

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
+//Front page button - splash page
 var tracksButton = document.getElementById("choose-tracks-button");
 
-// listeners for the front page button
-tracksButton.addEventListener("click", function(){
-  location.href = "playlist.html";
-});
+//Playlist div that populates albums
+var albumDiv = document.getElementById("album-div");
 
-tracksButton.addEventListener("mouseover", function(){
-  tracksButton.style.color = "white";
-  tracksButton.style.backgroundColor = "black";
-});
+//Album clicks - playlist
+var police = document.getElementById("album-0");
+var buhuru = document.getElementById("album-1");
+var pfloyd = document.getElementById("album-2");
+var mjackson = document.getElementById("album-3");
+var adele = document.getElementById("album-4");
 
-tracksButton.addEventListener("mouseleave", function(){
-  tracksButton.style.color = '';
-  tracksButton.style.backgroundColor = '';
-});
 
-//
+var clearTracksButton = document.getElementById("clear-tracks-button");
+var submitBinButton = document.getElementById("submit-bin-button");
+
+//Albums API
+var albumsURL = `https://lit-fortress-6467.herokuapp.com/object`;
+
+//test image
+var testimg = document.getElementById("test");
+
+
+
+// Listeners for playlist page
+window.onload = function(){
+  axios.get(albumsURL)
+  .then(function(response){
+    var data = response.data;
+    var resultsArr = data.results;
+    //console.log(resultsArr);
+
+    for(var i=0; i<resultsArr.length; i++){
+      // console.log(resultsArr[i]);
+      var albumCovers = resultsArr[i].cover_art;
+      // console.log(albumCovers);
+      albumDiv.innerHTML += `<div><img id=album-${i} class=album src=images/${albumCovers}></div>`
+    }
+  });
+}
+
+police.addEventListener("click", function(){
+  alert("police has been clicked");
+});
