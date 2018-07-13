a4f3b48af7c5c7a56077567a5dd1b19efd8b0a4a
Chelsea <cnoelle1091@gmail.com>
2018-07-12 19:34:50 -0700

    Populated playlist bin successfully

--- script.js
+++ script.js
@@ -4,42 +4,46 @@ var tracksButton = document.getElementById("choose-tracks-button");
 //Playlist div that populates albums
 var albumDiv = document.getElementById("album-div");
 
-//Album clicks - playlist
-var police = document.getElementById("album-0");
-var buhuru = document.getElementById("album-1");
-var pfloyd = document.getElementById("album-2");
-var mjackson = document.getElementById("album-3");
-var adele = document.getElementById("album-4");
-
-
+//Playlist buttons
 var clearTracksButton = document.getElementById("clear-tracks-button");
 var submitBinButton = document.getElementById("submit-bin-button");
+
 
 //Albums API
 var albumsURL = `https://lit-fortress-6467.herokuapp.com/object`;
 
-//test image
-var testimg = document.getElementById("test");
-
-
-
 // Listeners for playlist page
-window.onload = function(){
+window.addEventListener("load", function(){
   axios.get(albumsURL)
   .then(function(response){
     var data = response.data;
     var resultsArr = data.results;
-    //console.log(resultsArr);
+    // console.log(resultsArr);
 
     for(var i=0; i<resultsArr.length; i++){
-      // console.log(resultsArr[i]);
+      console.log(resultsArr[i]);
       var albumCovers = resultsArr[i].cover_art;
-      // console.log(albumCovers);
-      albumDiv.innerHTML += `<div><img id=album-${i} class=album src=images/${albumCovers}></div>`
+      var albumIDs = resultsArr[i].id;
+      albumDiv.innerHTML += `<div><img id=${albumIDs} class=album src=images/${albumCovers}></div>`
     }
-  });
-}
 
-police.addEventListener("click", function(){
-  alert("police has been clicked");
+
+    resultsArr.forEach(item => {
+      var albumArtist = item.artist;
+      var albumTitle = item.title;
+      var albumContentBin = document.getElementById("album-content-div");
+      var currentAlbum = document.getElementById(`${item.id}`);
+      currentAlbum.addEventListener("click", function(){
+        albumContentBin.innerHTML += `<p>${albumArtist}: ${albumTitle}</p>`
+      });
+    });
+
+    clearTracksButton.addEventListener("click", function(){
+      alert("clear tracks has been clicked");
+    });
+
+    submitBinButton.addEventListener("click", function(){
+      alert("submit bin clicked");
+    });
+  });
 });
